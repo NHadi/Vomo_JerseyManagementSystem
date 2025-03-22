86f3cab1d3741ce56c9046288803029d124e430b
nurul.hadi <nurul.hadi@outlook.com>
2025-03-23 01:04:31 +0700

    completed CRUD

--- Backend/internal/domain/menu/entity.go
+++ Backend/internal/domain/menu/entity.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Menu struct {
-	ID       int     `gorm:"primaryKey" json:"id"`
+	ID       int     `gorm:"primaryKey;autoIncrement:true;column:id" json:"id"`
 	Name     string  `gorm:"type:varchar(100);not null" json:"name"`
 	URL      string  `gorm:"type:varchar(255)" json:"url"`
 	Icon     string  `gorm:"type:varchar(50)" json:"icon"`
